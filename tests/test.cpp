fe973eb5130c930fb2f0c3731108a2f6bf5afbdb
ProkiProk <senior.prockuschev2017@yandex.ru>
2021-11-07 13:59:55 +0300

    v3.0

--- tests/test.cpp
+++ tests/test.cpp
@@ -37,3 +37,29 @@ TEST(Cache, reverseTest) {
     SUCCEED();
   }
 }
+
+TEST(Cache, fullDiagnosticsTest) {
+  CacheDiagnostics cd;
+  cd.full_diagnostics();
+  double time = cd.statistics[4].duration_random;
+  if (time == 0.0) {
+    FAIL();
+  } else {
+    SUCCEED();
+  }
+
+  time = cd.statistics[3].duration_front;
+  if (time == 0.0) {
+    FAIL();
+  } else {
+    SUCCEED();
+  }
+
+  time = cd.statistics[2].duration_reverse;
+  if (time == 0.0) {
+    FAIL();
+  } else {
+    SUCCEED();
+  }
+}
+
